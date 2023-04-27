1a02d45bbc5f70ceabd7d294c4d714f60d57310b
Your Name <you@example.com>
2023-04-27 13:39:37 -0500

    first commit

---
+++ student-sign-in-client/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+
+    <new-student-form v-on:student-added="newStudentAdded"></new-student-form>
+    <student-table 
+      v-bind:students="students" 
+      v-on:student-arrived-or-left="studentArrivedOrLeft"
+      v-on:delete-student="studentDeleted">
+    </student-table>
+
+    <student-message v-bind:student="mostRecentStudent"></student-message>
+
+  </div>
+</template>
+
+<script>
+
+import NewStudentForm from  './components/NewStudentForm.vue'
+import StudentMessage from  './components/StudentMessage.vue'
+import StudentTable from  './components/StudentTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NewStudentForm,
+    StudentMessage,
+    StudentTable
+  },
+  data() {
+    return{
+      students: [],
+      mostRecentStudent: {}
+    }
+  },
+  methods: {
+    newStudentAdded (student){
+      this.students.push(student)
+      this.students.sort(function(s1, s2){
+        return s1.name.toLowerCase() < s2.name.toLowerCase() ? -1 : 1
+      })
+    },
+    studentArrivedOrLeft(student, present) {
+      // find students in aaray of students
+      // update present attribute
+
+      let updateStudent = this.students.find(function(s) {
+        if (s.name === student.name && s.starID === student.starID){
+          // this is the student to update
+          return true
+        }
+      })
+
+      if (updateStudent){ // updates students
+        updateStudent.present = present
+        this.mostRecentStudent = updateStudent
+      }
+    },
+    studentDeleted(student){ // rename studentDeleted
+      // filter returns a new array of all students for whom the function returns true
+      this.students = this.students.filter(function(s) {
+        if (s != student){
+          return true
+        }
+
+      })
+
+      // clear welcome/goodbye
+      this.mostRecentStudent = {} // empty object
+    }
+  }
+}
+</script>
+
+<style>
+
+@import "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css";
+
+
+</style>
---
+++ student-sign-in-client/src/components/StudentRow.vue
@@ -0,0 +1,51 @@
+<template>
+    
+    <tr v-bind:class="{ present: student.present, absent: !student.present }"> 
+        <td>{{ student.name }}</td>
+        <td>{{ student.starID }}</td>
+        <td>
+        <input type="checkbox" v-on:change="arrivedOrLeft(student, $event.srcElement.checked)"></td>
+        <td v-show="edit"><img v-on:click="deleteStudent" src="@/assets/delete.png"></td> <!-- delte image-->
+        </tr>
+
+</template>
+
+<script>
+export default {
+    name: 'studentRow', //
+    props: {
+        student: Object,
+        edit: Boolean
+    },
+    methods: {
+        arrivedOrLeft(student, present){
+            //console.log("student row", student, present)
+            this.$emit('arrived-or-left', student, present) // send message to parent
+        },
+        deleteStudent() {
+            if (confirm(`Delete ${this.student.name}?`)) { 
+                this.$emit('delete-student', this.student)
+            }
+        }
+    }
+}
+
+</script>
+
+<style scoped> 
+
+.present {
+    color: gray;
+    font-style: italic;
+}
+
+.absent {
+    color: black;
+    font-weight: bold;
+}
+
+/* TODO set icon height*/
+img{
+    height: 25px;
+}
+</style>
